0fc9c347c3688b0460ce0072bad4818db9784f3b
Michael Combrink <u19009209@up.ac.za>
2022-10-02 14:46:45 +0200

    Dataservice class added

--- u19009209_HW05/Controllers/HomeController.cs
+++ u19009209_HW05/Controllers/HomeController.cs
@@ -12,10 +12,7 @@ namespace u19009209_HW05.Controllers
 {
     public class HomeController : Controller
     {
-        //Connection string
-        SqlConnection Dbconnection = new SqlConnection("Data Source=DESKTOP-3ML24ME\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True;");
-
-        
+        public DataService db = new DataService();
         public static List<BookHybrid> Books = new List<BookHybrid>();
         public static List<Student> Students = new List<Student>();
         public static List<Borrow> Borrows = new List<Borrow>();
@@ -29,36 +26,33 @@ namespace u19009209_HW05.Controllers
             List<BookHybrid> returnBooks = null;
             try
             {
-                clearLists();
-
-                
+                Books.Clear();
+                Borrows.Clear();
                 if (Searched != null)
                 {
                     returnBooks = Searched;
                 }
                 else
                 {
-                    getAllBooks();
-
-                    
-
-                    getAllStudents();
-
-                    
-
-                    getAllBorrows();
-
-                    
-
-                    UpdateBooks();
-
+                    Books = db.getAllBooks();
+                    Students = db.getAllStudents();
+                    Borrows = db.getAllBorrows();
+                    for (int i = 0; i < Borrows.Count; i++)
+                    {
+                        if (Borrows[i].broughtDate == null)
+                        {
+                            BookHybrid book = Books.Where(x => x.bookId == Borrows[i].bookId).FirstOrDefault();
+                            book.status = false;
+                            book.studentId = (int)Borrows[i].studentId;
+                        }
+                    }
                     returnBooks = Books;
                 }
 
-                ViewBag.Types = GetTypes();
 
-                ViewBag.Authors = GetAuthors();
+                ViewBag.Types = db.GetTypes();
 
+                ViewBag.Authors = db.GetAuthors();
 
             }
             catch (Exception message)
@@ -67,10 +61,17 @@ namespace u19009209_HW05.Controllers
             }
             finally
             {
-                Dbconnection.Close();
+                db.closeConnection();
             }
 
             return View(returnBooks);
+        }
+
+        [HttpGet]
+        public ActionResult Clear()
+        {
+            Searched.Clear();
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
@@ -83,51 +84,51 @@ namespace u19009209_HW05.Controllers
                 {
                     Searched.Clear();
                 }
-                if (name != "" && typeId != null && authorId != null)
+                else if (name != "" && typeId != null && authorId != null)
                 {
                     //search has to be on all 3 parameters
-                    getAllBooks();
+                    db.getAllBooks();
                     Searched = Books.Where(x => x.name == name && x.typeId == typeId && x.authorId == authorId).ToList();
                 }
                 else if (name != "" && typeId != null && authorId == null)
                 {
                     // search on name and type 
-                    getAllBooks();
+                    db.getAllBooks();
                     Searched = Books.Where(x => x.name == name && x.typeId == typeId).ToList();
                 }
                 else if (name != "" && typeId == null && authorId != null)
                 {
                     // search on name and author
-                    getAllBooks();
+                    db.getAllBooks();
                     Searched = Books.Where(x => x.name == name && x.authorId == authorId).ToList();
                 }
                 else if (name == "" && typeId != null && authorId != null)
                 {
                     //search on type and author
-                    getAllBooks();
+                    db.getAllBooks();
                     Searched = Books.Where(x => x.typeId == typeId && x.typeId == typeId).ToList();
                 }
                 else if (name == "" && typeId == null && authorId != null)
                 {
                     //search on author
-                    getAllBooks();
+                    db.getAllBooks();
                     Searched = Books.Where(x => x.authorId == authorId).ToList();
                 }
                 else if (name == "" && typeId != null && authorId == null)
                 {
                     // search on type
-                    getAllBooks();
+                    db.getAllBooks();
                     Searched = Books.Where(x => x.typeId == typeId).ToList();
                 }
                 else if (name != "" && typeId == null && authorId == null)
                 {
                     // search on name 
-                    getAllBooks();
+                    db.getAllBooks();
                     Searched = Books.Where(x => x.name == name).ToList();
                 }
                 else
                 {
-                    TempData["Message"] = "You Didnt Search For Anything Stop Wasting my Time !!!";
+                    TempData["Message"] = "Search field empty. Please enter some values.";
                 }
             }
             catch (Exception message)
@@ -184,135 +185,6 @@ namespace u19009209_HW05.Controllers
             return View(Students);
         }
 
-
-
-
-        private void clearLists()
-        {
-            Books.Clear();
-            Borrows.Clear();
-        }
-
-        private void getAllBooks()
-        {
-            SqlCommand getAllBooks = new SqlCommand("SELECT book.[bookId] as bookId ,book.[name] as name ,book.[pagecount] as pagecount ,book.[point] as point, auth.[surname] as authorSurname ,type.[name] typeName,  book.[authorId],book.[typeId] " +
-                                        "FROM [Library].[dbo].[books] book " +
-                                        "JOIN [Library].[dbo].[authors] auth on book.authorId = auth.authorId " +
-                                        "JOIN [Library].[dbo].[types] type on book.typeId = type.typeId",
-                                        Dbconnection);
-            Dbconnection.Open();
-            SqlDataReader readBooks = getAllBooks.ExecuteReader();
-            while (readBooks.Read())
-            {
-                BookHybrid book = new BookHybrid();
-                book.bookId = (int)readBooks["bookId"];
-                book.name = (string)readBooks["name"];
-                book.pagecount = (int)readBooks["pagecount"];
-                book.point = (int)readBooks["point"];
-                book.authorId = (int)readBooks["authorId"];
-                book.typeId = (int)readBooks["typeId"];
-                book.authorSurname = (string)readBooks["authorSurname"];
-                book.typeName = (string)readBooks["typeName"];
-                book.status = true;
-                Books.Add(book);
-            }
-            Dbconnection.Close();
-        }
-
-        private void getAllStudents()
-        {
-            SqlCommand getAllStudents = new SqlCommand("SELECT * FROM [Library].[dbo].[students]", Dbconnection);
-            Dbconnection.Open();
-            SqlDataReader readStudents = getAllStudents.ExecuteReader();
-            while (readStudents.Read())
-            {
-                Student student = new Student();
-                student.studentId = (int)readStudents["studentId"];
-                student.name = (string)readStudents["name"];
-                student.surname = (string)readStudents["surname"];
-                student.birthdate = (DateTime)readStudents["birthdate"];
-                student.gender = (string)readStudents["gender"];
-                student.Class = (string)readStudents["class"];
-                student.point = (int)readStudents["point"];
-                Students.Add(student);
-            }
-            Dbconnection.Close();
-        }
-
-        private void getAllBorrows()
-        {
-            SqlCommand getBorrows = new SqlCommand("SELECT * FROM [Library].[dbo].[borrows]", Dbconnection);
-            Dbconnection.Open();
-            SqlDataReader readBorrows = getBorrows.ExecuteReader();
-            while (readBorrows.Read())
-            {
-                Borrow borrow = new Borrow();
-                borrow.borrowId = (int)readBorrows["borrowId"];
-                borrow.studentId = (int)readBorrows["studentId"];
-                borrow.bookId = (int)readBorrows["bookId"];
-                borrow.takenDate = Convert.ToDateTime(readBorrows["takenDate"]);
-                var broughtDate = readBorrows["broughtDate"].ToString();
-                if (broughtDate != "")
-                {
-                    borrow.broughtDate = Convert.ToDateTime(readBorrows["broughtDate"]);
-                }
-                else
-                {
-                    borrow.broughtDate = null;
-                }
-
-                Borrows.Add(borrow);
-            }
-            Dbconnection.Close();
-        }
-
-        private void UpdateBooks()
-        {
-
-            for (int i = 0; i < Borrows.Count; i++)
-            {
-                if (Borrows[i].broughtDate == null)
-                {
-                    BookHybrid book = Books.Where(x => x.bookId == Borrows[i].bookId).FirstOrDefault();
-                    book.status = false;
-                    book.studentId = (int)Borrows[i].studentId;
-                }
-            }
-
-        }
-
-
-        private SelectList GetTypes()
-        {
-            List<type> types = new List<type>();
-            SqlCommand getAllTypes = new SqlCommand("SELECT * FROM [Library].[dbo].[types]", Dbconnection);
-            Dbconnection.Open();
-            SqlDataReader readTypes = getAllTypes.ExecuteReader();
-            while (readTypes.Read())
-            {
-                type type = new type();
-                type.typeId = (int)readTypes["typeId"];
-                type.name = (string)readTypes["name"];
-                types.Add(type);
-            }
-            Dbconnection.Close();
-            return new SelectList(types, "typeId", "name");
-        }
-
-        private SelectList GetAuthors()
-        {
-            List<Author> authors = new List<Author>();
-            SqlCommand getAllAuthors = new SqlCommand("SELECT * FROM [Library].[dbo].[authors]", Dbconnection);
-            Dbconnection.Open();
-            SqlDataReader readAuthors = getAllAuthors.ExecuteReader();
-            while (readAuthors.Read())
-            {
-                Author author = new Author();
-                author.authorId = (int)readAuthors["authorId"];
-                author.name = (string)readAuthors["name"];
-                authors.Add(author);
-            }
-            return new SelectList(authors, "authorId", "name");
-        }
+        
     }
 }
---
+++ u19009209_HW05/Models/DataService.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data.Common;
+using u19009209_HW05.Models.ViewModels;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace u19009209_HW05.Models
+{
+    public class DataService
+    {
+
+        private static DataService instance;
+        public static DataService GetDataService()
+        {
+            if (instance == null)
+            {
+                instance = new DataService();
+            }
+            return instance;
+        }
+        private SqlConnection buildConnection()
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+            sqlConnectionStringBuilder["Data Source"] = "DESKTOP-3ML24ME\\SQLEXPRESS";
+            sqlConnectionStringBuilder["Initial Catalog"] = "Library";
+            sqlConnectionStringBuilder["IntergratedSecurity"] = ".";
+            return new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+        }
+
+        public bool openConnection()
+        {
+            using (SqlConnection conn = buildConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+        public bool closeConnection()
+        {
+            using (SqlConnection conn = buildConnection())
+            {
+                try
+                {
+                    conn.Close();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        public List<BookHybrid> getAllBooks()
+        {
+            List<BookHybrid> bookList = new List<BookHybrid>();
+            String command = "SELECT book.[bookId] as bookId ,book.[name] as name ,book.[pagecount] as pagecount ,book.[point] as point, auth.[surname] as authorSurname ,type.[name] typeName,  book.[authorId],book.[typeId] " +
+                            "FROM [Library].[dbo].[books] book " +
+                            "JOIN [Library].[dbo].[authors] auth on book.authorId = auth.authorId " +
+                            "JOIN [Library].[dbo].[types] type on book.typeId = type.typeId";
+            using (SqlConnection conn = buildConnection())
+            {
+                try
+                {
+                    openConnection();
+                    using (SqlCommand cmd = new SqlCommand(command, conn))
+                    {
+                        SqlDataReader readBooks = cmd.ExecuteReader();
+                        while (readBooks.Read())
+                        {
+                            BookHybrid book = new BookHybrid();
+                            book.bookId = (int)readBooks["bookId"];
+                            book.name = (string)readBooks["name"];
+                            book.pagecount = (int)readBooks["pagecount"];
+                            book.point = (int)readBooks["point"];
+                            book.authorId = (int)readBooks["authorId"];
+                            book.typeId = (int)readBooks["typeId"];
+                            book.authorSurname = (string)readBooks["authorSurname"];
+                            book.typeName = (string)readBooks["typeName"];
+                            book.status = true;
+                            bookList.Add(book);
+                        }
+                    }
+                    closeConnection();
+                }
+                catch
+                {
+
+                }
+            }
+            return bookList;
+        }
+
+        public List<Student> getAllStudents()
+        {
+            List<Student> studentList = new List<Student>();
+            String command = "SELECT * FROM [Library].[dbo].[students]";
+            using (SqlConnection conn = buildConnection())
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(command, conn))
+                {
+                    SqlDataReader readStudents = cmd.ExecuteReader();
+                    while (readStudents.Read())
+                    {
+                        Student student = new Student();
+                        student.studentId = (int)readStudents["studentId"];
+                        student.name = (string)readStudents["name"];
+                        student.surname = (string)readStudents["surname"];
+                        student.birthdate = (DateTime)readStudents["birthdate"];
+                        student.gender = (string)readStudents["gender"];
+                        student.Class = (string)readStudents["class"];
+                        student.point = (int)readStudents["point"];
+                        studentList.Add(student);
+                    }
+                }
+            }
+            return studentList;
+        }
+
+        public List<Borrow> getAllBorrows()
+        {
+            List<Borrow> borrowList = new List<Borrow>();
+            String command = "SELECT * FROM [Library].[dbo].[borrows]";
+            using (SqlConnection conn = buildConnection())
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(command, conn))
+                {
+                    SqlDataReader readBorrows = cmd.ExecuteReader();
+                    while (readBorrows.Read())
+                    {
+                        Borrow borrow = new Borrow();
+                        borrow.borrowId = (int)readBorrows["borrowId"];
+                        borrow.studentId = (int)readBorrows["studentId"];
+                        borrow.bookId = (int)readBorrows["bookId"];
+                        borrow.takenDate = Convert.ToDateTime(readBorrows["takenDate"]);
+                        var broughtDate = readBorrows["broughtDate"].ToString();
+                        if (broughtDate != "")
+                        {
+                            borrow.broughtDate = Convert.ToDateTime(readBorrows["broughtDate"]);
+                        }
+                        else
+                        {
+                            borrow.broughtDate = null;
+                        }
+
+                        borrowList.Add(borrow);
+                    }
+                }
+            }
+            return borrowList;
+        }
+
+        public SelectList GetTypes()
+        {
+            List<type> typesList = new List<type>();
+            String command = "SELECT * FROM [Library].[dbo].[types]";
+            using (SqlConnection conn = buildConnection())
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(command, conn))
+                {
+                    SqlDataReader readTypes = cmd.ExecuteReader();
+                    while (readTypes.Read())
+                    {
+                        type type = new type();
+                        type.typeId = (int)readTypes["typeId"];
+                        type.name = (string)readTypes["name"];
+                        typesList.Add(type);
+                    }
+                }
+            }
+            return new SelectList(typesList, "typeId", "name");
+        }
+
+        public SelectList GetAuthors()
+        {
+            List<Author> authorsList = new List<Author>();
+            String command = "SELECT * FROM [Library].[dbo].[authors]";
+            using (SqlConnection conn = buildConnection())
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(command, conn))
+                {
+                    SqlDataReader readAuthors = cmd.ExecuteReader();
+                    while (readAuthors.Read())
+                    {
+                        Author author = new Author();
+                        author.authorId = (int)readAuthors["authorId"];
+                        author.name = (string)readAuthors["name"];
+                        authorsList.Add(author);
+                    }
+                }
+            }
+            return new SelectList(authorsList, "typeId", "name");
+        }
+    }
+}
